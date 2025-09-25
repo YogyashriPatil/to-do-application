aabe2f893de40655e48bca8792709f9940550f52
YogyashriPatil <yogyashripatil20@gmail.com>
2025-09-25 13:21:12 +0530

    Create a frontend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,6 +2,8 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import { CreateTodo } from './components/CreateTodo'
+import { Todos } from './components/Tofos'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -9,7 +11,8 @@ function App() {
   return (
   
       <div>
-        
+        <CreateTodo></CreateTodo>
+        <Todos></Todos>
       </div>
      
   )
---
+++ frontend/src/components/Tofos.jsx
@@ -0,0 +1,8 @@
+
+export function Todos() {
+    return <div>
+        <h1>Go to Gym </h1>
+        <h2> You need to go Gym</h2>
+        <button> Mark as Completed</button>
+    </div>
+}
